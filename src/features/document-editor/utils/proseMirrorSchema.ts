b4c68008c27289d22081a55c6e8f2c4039a236c9
Juan Martín Olivas <juanmarolivas@gmail.com>
2026-03-24 20:29:45 +0100

    feat: add base styling for editor

--- src/features/document-editor/utils/proseMirrorSchema.ts
+++ src/features/document-editor/utils/proseMirrorSchema.ts
@@ -1,39 +1,39 @@
-import { Schema } from 'prosemirror-model'
-import { addListNodes } from 'prosemirror-schema-list'
-import { schema as basicSchema } from 'prosemirror-schema-basic'
+import { Schema } from "prosemirror-model";
+import { addListNodes } from "prosemirror-schema-list";
+import { schema as basicSchema } from "prosemirror-schema-basic";
 
-const nodes = addListNodes(basicSchema.spec.nodes, 'paragraph block*', 'block')
+const nodes = addListNodes(basicSchema.spec.nodes, "paragraph block*", "block");
 
 export const documentEditorSchema = new Schema({
   nodes,
   marks: basicSchema.spec.marks
-    .addToEnd('underline', {
-      parseDOM: [{ tag: 'u' }, { style: 'text-decoration=underline' }],
+    .addToEnd("underline", {
+      parseDOM: [{ tag: "u" }, { style: "text-decoration=underline" }],
       toDOM() {
-        return ['u', 0]
+        return ["u", 0];
       },
     })
-    .addToEnd('strike', {
+    .addToEnd("strike", {
       parseDOM: [
-        { tag: 's' },
-        { tag: 'del' },
-        { tag: 'strike' },
-        { style: 'text-decoration=line-through' },
+        { tag: "s" },
+        { tag: "del" },
+        { tag: "strike" },
+        { style: "text-decoration=line-through" },
       ],
       toDOM() {
-        return ['s', 0]
+        return ["s", 0];
       },
     })
-    .addToEnd('subscript', {
-      parseDOM: [{ tag: 'sub' }, { style: 'vertical-align=sub' }],
+    .addToEnd("subscript", {
+      parseDOM: [{ tag: "sub" }, { style: "vertical-align=sub" }],
       toDOM() {
-        return ['sub', 0]
+        return ["sub", 0];
       },
     })
-    .addToEnd('superscript', {
-      parseDOM: [{ tag: 'sup' }, { style: 'vertical-align=super' }],
+    .addToEnd("superscript", {
+      parseDOM: [{ tag: "sup" }, { style: "vertical-align=super" }],
       toDOM() {
-        return ['sup', 0]
+        return ["sup", 0];
       },
     }),
-})
+});
